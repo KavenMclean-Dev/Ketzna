b2f92931f5393235d744ff12cb4e0ab820ff40e8
Administrator <Administrator@DESKTOP-9HU6DH7>
2025-03-27 16:14:28 +0200

    Add project files.

---
+++ KetznaHome/Pages/Abuot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KetznaHome.Pages
+{
+    public class AbuotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KetznaHome/Pages/Blogs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KetznaHome.Pages
+{
+    public class BlogsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
